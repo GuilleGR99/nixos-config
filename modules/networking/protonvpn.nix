119df282b68a58d2315b91713d1907d1c3e8b0fb
Guillermo <rainguru@proton.me>
2026-07-11 12:41:49 +0200

    fix(network): route Tailscale traffic outside ProtonVPN tunnel

--- modules/networking/protonvpn.nix
+++ modules/networking/protonvpn.nix
@@ -3,5 +3,7 @@
 {
   networking.wg-quick.interfaces.proton = {
     configFile = "/etc/wireguard/proton.conf";
+    postUp = "ip route add 100.64.0.0/10 dev tailscale0";
+    postDown = "ip route del 100.64.0.0/10 dev tailscale0";
   };
 }
