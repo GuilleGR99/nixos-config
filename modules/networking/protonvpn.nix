2723b210d9af59db5ca28c3f69101c04127e9031
Guillermo <rainguru@proton.me>
2026-07-15 21:33:24 +0200

    fix(network): start ProtonVPN after Tailscale

--- modules/networking/protonvpn.nix
+++ modules/networking/protonvpn.nix
@@ -1,7 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   networking.wg-quick.interfaces.proton = {
     configFile = "/etc/wireguard/proton.conf";
+  };
+
+  systemd.services.wg-quick-proton = {
+    wants = [ "tailscaled.service" ];
+    after = [ "tailscaled.service" ];
   };
 }
